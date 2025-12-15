46e8b6f7f124f5331e9fb62cd63c5520b866cbf9
Shin-zuo <jeffreypalad14@gmail.com>
2025-12-15 15:46:52 +0800

    with migration practice

--- database/migrations/2025_12_15_030012_create_chirps_table.php
+++ database/migrations/2025_12_15_030012_create_chirps_table.php
@@ -11,24 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('chirps', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->string('message', 255);
             $table->timestamps();
         });
     }
-
- 
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('chirps');
     }
 };
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,18 @@
 <x-layout>
      <div class="max-w-3xl mx-auto">
+        @foreach ($chirps as $chirp)
+            
+     
             <div class="card bg-base-100 shadow mt-8" style="border-radius: 12px; background-color:white;">
                 <div class="card-body">
                     <div>
-                        <h1 class="card-title text-3xl font-bold mb-4">Welcome to Chirper!</h1>
-                        <p class="mb-4">Chirper is a simple and fun social media platform where you can share your
-                            thoughts in
-                            short messages called "chirps". Join our community and start chirping today!</p>
+                        <div class="font-semibold">{{ $chirp['author'] }}</div>
+                        <div class="mt-2">{{ $chirp['message'] }}</div>
+                        <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
                     </div>
                 </div>
             </div>
+               @endforeach
         </div>
+        
 </x-layout>
